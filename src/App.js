9417e135d455ad7e8845676ec97904070a2677e9
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-01-18 23:50:13 -0800

    add todo list and slice

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Counter from './Components/Counter'
+import Todo from './Components/Todo';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
